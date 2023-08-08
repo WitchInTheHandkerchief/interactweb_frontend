f4a00f8eabb55899a2ebcc6800ccd630d7186cef
Bayel Chief <marvel.vovo@gmail.com>
2023-08-08 22:03:12 +0600

    added activity page

---
+++ src/pages/activityPage/Activity.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+const ActivityPage = (props) => {
+   
+    return (
+        <>
+        <Header />
+
+        <Footer /> 
+           
+        
+        </>
+   )
+}
+
+
+
+
+export default ActivityPage; 
