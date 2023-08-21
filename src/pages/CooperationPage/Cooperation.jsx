00923baaf3923c4d59820aac89be5062cd1f9c79
Bayel Chief <marvel.vovo@gmail.com>
2023-08-21 22:18:49 +0600

    added style for cooperation page

--- src/pages/CooperationPage/Cooperation.jsx
+++ src/pages/CooperationPage/Cooperation.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import classes from "./cooperation.module.css";
 const CooperationPage = () => {
    
     return (
         <div className={classes.main_coop}>
             <div className={classes.collaboration}>
-                <div className="collaboration__title">
+                <div className={classes.collaboration__title}>
                     Сотрудничество с нами
                 </div>
-                <div className="collaboration__descrp">
+                <div className={classes.collaboration__descrp}>
                     <p>Сотрудничество — ключевой элемент нашей организации. 
                         Мы открыты для партнерства с другими волонтерскими организациями, 
                         некоммерческими учреждениями, государственными структурами и частными компаниями, 
@@ -18,14 +19,14 @@ const CooperationPage = () => {
             </div>
 
             <div className={classes.assets}>
-                <img src="" alt="" />
+           
             </div>
 
             <div className={classes.collaborationWithUs}>
-                <div className="collaborationWithUs__title">
+                <div className={classes.collaborationWithUs__title}>
                     Как начать с нами сотрудничать?
                 </div>
-                <div className="collaborationWithUs__descrp">
+                <div className={classes.collaborationWithUs__descrp}>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                         Etiam quis placerat risus, ac ornare magna. 
                         Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
@@ -38,14 +39,14 @@ const CooperationPage = () => {
             </div>
 
             <div className={classes.linkTo_contacts}>
-                Все доступные способы связи с нами находятся на странице Контакты
+                Все доступные способы связи с нами находятся на странице <Link className={classes.link} to='/contacts'>Контакты</Link>
             </div>
 
             <div className={classes.collaborationWithUsAsVolunteer}>
-                <div className="collaborationWithUsAsVolunteer__title">
+                <div className={classes.collaborationWithUsAsVolunteer__title}>
                     Как стать нашим волонтером?
                 </div>
-                <div className="collaborationWithUsAsVolunteer__descrp">
+                <div className={classes.collaborationWithUsAsVolunteer__descrp}>
                     <p>Мы всегда открыты для новых и стремительных личностей, 
                         именно поэтому мы устраиваем наборы в нашу команду два раза в год. 
                         Информация о набере выходит у нас на официальном инстаграм аккаунте и 
